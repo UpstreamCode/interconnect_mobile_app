6399f9c72b1365178bf754bd2b957af6269316b2
Kristin <sindorf_kristin@bah.com>
2020-04-01 16:28:46 -0400

    Credit where credit is due

--- lib/meetups/chat_page.dart
+++ lib/meetups/chat_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:interconnect_mobile_app/meetups/chat_message.dart';
 import 'package:interconnect_mobile_app/meetups/chat_args.dart';
 
+/// Chat Feature based off https://github.com/flutter/friendlychat-steps
+
 class ChatPage extends StatefulWidget {
   @override
   State createState() => new ChatPageState();
