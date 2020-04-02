952828670d1cc59d949a1e2b788a9e922f17ffba
Kristin <sindorf_kristin@bah.com>
2020-04-02 08:46:50 -0400

    Add TODO with color codes from design mockups

--- lib/constants/theme_colors.dart
+++ lib/constants/theme_colors.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// TODO: use design colors from mockups
+/// overlay = 0x4071C9BF
+/// darkAccent = 0xFF707070
+/// lightAccent = 0xFFF3B651
+/// darkPrimary = 0xFF18989B
+/// medPrimary = 0xFF71C9BF
+/// lightPrimary = 0xFFDBF1EF
+/// gradient = 0xFF71C9BF - 0xFF18989B
+
 class ThemeColors {
   static const backgroundColor = Colors.white;
   static const primary = Colors.teal;
