cea8abde0754514ab961be0e43730849659ec75a
Kristin <sindorf_kristin@bah.com>
2020-04-04 14:59:38 -0400

    Ternary for placeholders vs user avatars

--- lib/meetups/matches_grid.dart
+++ lib/meetups/matches_grid.dart
@@ -6,20 +6,27 @@ class MatchesGrid extends StatelessWidget {
   final List<Person> matches;
   const MatchesGrid({Key key, @required this.matches}) : super(key: key);
 
+  static const List placeholders = [
+    'images/avatar.png',
+    'images/avatar.png',
+    'images/avatar.png',
+    'images/avatar.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
+    List list = matches.isNotEmpty ? matches : placeholders;
     return GridView.builder(
       primary: false,
       padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
       gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2),
       // Let the ListView know how many items it needs to build.
-      itemCount: matches.length,
+      itemCount: list.length,
       // Provide a builder function. This is where the magic happens.
       // Convert each item into a widget based on the type of item it is.
       itemBuilder: (context, index) {
-        final item = matches[index];
-
+        final item = list[index];
         return Center(
           child: new Container(
             width: 100.0,
@@ -34,7 +41,7 @@ class MatchesGrid extends StatelessWidget {
                 ),
                 shape: BoxShape.circle,
                 image: new DecorationImage(
-                    image: new AssetImage('images/avatar.png'),
+                    image: new AssetImage(item is String ? item : item.image),
                 )
               ),
             ),
